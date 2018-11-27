7913597aee9dec5acc8445e9826ed93983f44f07
caiodasilva <caio@nave.rs>
2018-11-26 23:40:12 -0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,91 @@
+import React, { Component, Fragment } from 'react'
+import { Header, Main, TextArea, Left, Right, Button, Button2, Games, List, Item1, Item2, Item3, Row } from './lib/index'
+
+class App extends Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      show: false,
+      list: [],
+      games: []
+    }
+  }
+
+  onBack () {
+    this.setState({ show: false })
+  }
+
+  async onSubmit (event) {
+    event.preventDefault()
+    const form = event.target
+    const players = form.item.value.split('\n')
+    if (players) {
+      await this.setState({ list: [...players] })
+    }
+
+    for (let i = 0; i < this.state.list.length; i++) {
+      for (let j = i + 1; j < this.state.list.length; j++) {
+        if (this.state.list[i] && this.state.list[j]) {
+          await this.setState({
+            games: [
+              ...this.state.games,
+              `${this.state.list[i]} - ${this.state.list[j]}`
+            ],
+            show: true
+          })
+        }
+      }
+    }
+    form.reset()
+  }
+
+  render () {
+    const { show, games } = this.state
+    return (
+      <Fragment>
+        <Header>
+          <h1>Gerador de tabelas todos contra todos</h1>
+        </Header>
+        {
+          show ? (
+            <Games>
+              <Button2 onClick={this.onBack.bind(this)}> Voltar </Button2>
+              <Button2> Imprimir </Button2>
+              <List>
+                {
+                  games.map((item) => {
+                    const game = item.split(' ')
+                    return (
+                      <Row>
+                        <Item1> {game[0]} </Item1>
+                        <Item2> {game[1]} </Item2>
+                        <Item3> {game[2]} </Item3>
+                      </Row>
+                    )
+                  })
+                }
+
+              </List>
+            </Games>
+          )
+            : (<Main>
+              <Left>
+                <p> Nome dos jogadores </p>
+                <form onSubmit={this.onSubmit.bind(this)}>
+                  <TextArea type='text' name='item' placeholder='Um jogador em cada linha' autoFocus />
+                  <Button> Gerar Partidas</Button>
+                </form>
+              </Left>
+              <Right>
+                <p> Digite os nomes dos times, um em cada linha, e aperte o botão.</p>
+                <p> A tabela de todos-contra-todos será gerada, com os jogos balanceadamente distribuídos entre as rodadas. Pronta para ser impressa.</p>
+              </Right>
+            </Main>)
+        }
+      </Fragment>
+    )
+  }
+}
+
+export default App
---
+++ src/lib/index.js
@@ -0,0 +1,95 @@
+import styled from 'styled-components'
+
+export const Background = styled.div`
+  background-color: #3A394D
+  height: 100vh
+  display: flex
+  flex: 1
+  justify-content: center
+  align-items: center
+`
+export const Main = styled.div`
+  display: flex
+  margin: auto
+  margin-top: 150px
+  width: 70%
+`
+export const Left = styled.div`
+  flex:1 
+`
+
+export const Right = styled.div`
+  flex:1
+  padding: 10px
+`
+export const TextArea = styled.textarea`
+  width: 70%
+  height: 240px
+  border-radius: 5px
+  padding: 10px
+`
+
+export const Header = styled.div`
+  background-color: #34394d
+  color: white
+  position: absolute
+  top: 0
+  width: 100%
+  padding:25px
+  text-align: center
+`
+export const Games = styled.div`
+  margin: auto
+  margin-top: 150px
+  width: 70%
+  text-align: center
+`
+
+export const Button = styled.button`
+  margin:auto
+  color: white
+  background-color: #3A394D
+  border-radius: 3px
+  border: none
+  outline: none
+  width: 75%
+  text-align: center
+  padding: 10px
+  margin-top: 20px
+`
+export const Button2 = styled.button`
+  margin:auto
+  margin-left: 10px
+  margin-right: 10px
+  color: white
+  background-color: #3A394D
+  border-radius: 3px
+  border: none
+  outline: none
+  width: 20%
+  padding: 10px
+  margin-top: 20px
+`
+export const List = styled.div`
+  display: flex
+  flex-direction: column
+  margin-top : 40px
+`
+
+export const Row = styled.div`
+  display: flex
+  flex-direction: row
+`
+
+export const Item1 = styled.label`
+  font-size: 30px
+  flex:1
+`
+export const Item2 = styled.label`
+  font-size: 30px
+  flex:1
+`
+export const Item3 = styled.label`
+  font-size: 30px
+  flex:1
+`
